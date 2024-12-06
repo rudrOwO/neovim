a3f0d8dea150d64d19a6c0882a734ca5884db79f
Dardaul Hoque <rudro1152@gmail.com>
2024-12-06 10:24:20 +0600

    revert colorscheme changes from 8f7aac5

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,7 @@
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-local function is_wsl()
-  local uname = vim.fn.system("uname -r")
-  return string.find(uname, "WSL2")
-end
-
 return {
-  { "navarasu/onedark.nvim", opts = { style = "darker", transparent = is_wsl() } },
-  { "EdenEast/nightfox.nvim", opts = { options = { transparent = is_wsl() } } },
+  { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  { "EdenEast/nightfox.nvim" },
   {
     "lazyvim/lazyVim",
     opts = { colorscheme = "carbonfox" },
